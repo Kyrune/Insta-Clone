7cead94509aeb3b05f857201a712f50579e99ba2
Kyrune <jn-01@centurytel.net>
2021-02-22 16:02:47 -0600

    Create pages

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 export const DASHBOARD = '/';
 export const LOGIN = '/login';
-export const SIGNUP = 'sighup';
+export const SIGN_UP = 'sighup';
 export const PROFILE = '/p/:uername';
 export const NOT_FOUND = '/not-found';
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
-    filename: "index.bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -16,15 +16,15 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+      template: './public/index.html'
+    })
+  ]
 };
