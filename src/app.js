266988e8e58c57a3404744909d1dc404dc747e5e
Kyrune <jn-01@centurytel.net>
2021-02-22 16:02:58 -0600

    Add routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 import React, { lazy, Suspense } from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 
-ReactDom.render(<App />, document.getElementById("root"))
+// Lazy page - not imported until user hits it.
+const Dashboard = lazy(() => import ('./pages/dashboard'));
+const Login = lazy (() => import ('./pages/login'));
+const SignUp = lazy (() => import ('./pages/signup'));
+const Profile = lazy (() => import ('./pages/profile'));
+const NotFound = lazy (() => import ('./pages/not-found'));
+
+export default function App() {
+    return (
+        <Router>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Switch>
+                    <Route path={ROUTES.LOGIN} component={Login} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                    <Route path={ROUTES.PROFILE} component={Profile} />
+                    <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+                    <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+                </Switch>
+            </Suspense>
+        </Router>
+    );
+}
