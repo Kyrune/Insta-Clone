351145bae57482c7be312df3c87cdc757a439d8f
Kyrune <jn-01@centurytel.net>
2021-02-22 16:52:04 -0600

    Fix signup typo

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 export const DASHBOARD = '/';
 export const LOGIN = '/login';
-export const SIGN_UP = 'sighup';
+export const SIGN_UP = 'signup';
 export const PROFILE = '/p/:uername';
 export const NOT_FOUND = '/not-found';
