64e7fa1dfc2dc67d9aa328cc6d3d61d6eef9f738
Kyrune <jn-01@centurytel.net>
2021-02-23 13:40:40 -0600

    Import Skeleton/Post

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import Post from './post';
+import useFollowedUsersPhotos from '../hooks/use-followed-users-photos';
 
 export default function Timeline() {
+    const { photos } = useFollowedUsersPhotos();
+    
     return (
-        <p>I am a timeline</p>
-    )
+        <div className="container col-span-2">
+            {!photos ? (
+                <>
+                    {[...new Array(4)].map((_, index) => (
+                        <Skeleton key={index} count={1} width={320} height={400} />
+                    ))}
+                </>
+            ) : photos && photos.length > 0 ? (
+                photos.map((content) => <Post key={content.docId} content={content} />)
+            ) : (
+                <p className="text-center text-2xl">Follow people to see photos!</p>
+            )}
+        </div>
+    );
 }
