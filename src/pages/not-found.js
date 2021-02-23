f56d66f041c84679fe5b3647adc97b22b2066118
Kyrune <jn-01@centurytel.net>
2021-02-22 20:06:30 -0600

    Add not-found JSX

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function NotFound() {
+    useEffect(() => {
+        document.title = '404 - Not Found';
+    });
+
     return (
-        <p>Hello from NotFound</p>
+        <div className="bg-gray-100">
+            <div className="mx-auto max-w-screen-lg">
+                <p className="text-center text-2xl">Not Found</p>
+            </div>
+        </div>
     )
 }
