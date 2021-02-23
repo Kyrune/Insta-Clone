fe0447815fb9475abe07f27cfdc51519632cc27d
Kyrune <jn-01@centurytel.net>
2021-02-23 09:33:15 -0600

    Add Tailwind classes

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 
 export default function Header() {
     return (
-        <p>I am a header</p>
+        <header className="h-16 bg-white border-b mb-8">
+            <div className="container mx-auto max-width-lg h-full">
+                <div className="flex justify-between h-full">
+                    <div className="text-gray-700 text-center felx items-center align-items cursor-pointer">
+                        <h1>Instagram</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
