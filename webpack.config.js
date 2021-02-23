89f2f2839da82eb4054832cd559895675ae2fe01
Kyrune <jn-01@centurytel.net>
2021-02-23 12:01:49 -0600

    Install packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   mode: 'development',
+  resolve: {
+    fallback: {
+      'path': require.resolve('path-browserify'),
+      'fs': false,
+      'tls': false,
+      'net': false,
+      'path': false,
+      'zlib': false,
+      'http': false,
+      'https': false,
+      'stream': false,
+      'crypto': false,
+      'crypto-browserify': require.resolve('crypto-browserify')
+    }
+  },
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
@@ -22,6 +38,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new NodePolyfillPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html'
