faf19b4867717d4e265a20fa68b342a5ee4fb953
Kyrune <jn-01@centurytel.net>
2021-02-22 20:06:13 -0600

    Add header/timeline/sidebar components

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/header';
+import Timeline from '../components/timeline';
+import Sidebar from '../components/sidebar/index';
 
 export default function Dashboard() {
+    useEffect(() => {
+        document.title = 'Instagram';
+    }, []);
     return (
-        <p>Hello from Dashboard</p>
+        <div className="bg-gray-100">
+            <Header />
+            <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+                <Timeline />
+                <Sidebar />
+            </div>
+        </div>
     )
 }
