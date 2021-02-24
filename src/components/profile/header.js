bb6a89e2105a57b4319dbd0d612b62ad3e22dd12
Kyrune <jn-01@centurytel.net>
2021-02-24 15:25:27 -0600

    Create file

---
+++ src/components/profile/header.js
@@ -0,0 +1,13 @@
+import React, { useState, UseEffect } from 'react';
+
+export default function Header({
+    photosCount,
+    followerCount,
+    setFollowerCount,
+    profile,
+    username
+}) {
+    return (
+        <p>Hello</p>
+    )
+}
