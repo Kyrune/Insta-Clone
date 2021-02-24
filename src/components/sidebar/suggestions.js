87f8df7bad2ad6465835778982f44e60ddeee03f
Kyrune <jn-01@centurytel.net>
2021-02-24 13:12:14 -0600

    Remove useState data

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -4,31 +4,17 @@ import SuggestedProfile from './suggested-profile';
 import { getSuggestedProfiles } from '../../services/firebase';
 
 const Suggestions = ({ userId }) => {
-    const [profiles, setProfiles] = useState([
-        {
-            docId: '1',
-            userDocId: 1,
-            username: 'dali',
-            profileId: '2',
-            userId: '3'
-        },
-        {
-            docId: '2',
-            userDocId: 1,
-            username: 'orwell',
-            profileId: '2',
-            userId: '3'
-        }
-    ]);
+    const [profiles, setProfiles] = useState(null);
+
 
     useEffect(() => {
         async function suggestedProfiles() {
             const response = await getSuggestedProfiles(userId);
             setProfiles(response);
         }
-        // if (userId) {
-        //     suggestedProfiles();
-        // }
+        if (userId) {
+            suggestedProfiles();
+        }
     }, [userId]);
 
     return !profiles ? (
