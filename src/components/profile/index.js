e65333bed38f074f8302d1cd1e60a89d04f88ea7
Kyrune <jn-01@centurytel.net>
2021-02-24 14:30:15 -0600

    Create useEffect functionality

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,12 +1,13 @@
 import React, { useReducer, useEffect } from 'react';
-import Header from '../post/header';
+import Header from './header';
 import Photos from './photos';
+import { getUserByUsername, getUserPhotosByUsername } from '../../services/firebase';
 
 const reducer = (state, newState) => ({ ...state, ...newState });
 const initialState = {
     profile: {},
     photosCollection: [],
-    followerCount
+    followerCount: 0
 };
 
 export default function Profile({ username }) {
@@ -14,6 +15,14 @@ export default function Profile({ username }) {
         reducer,
         initialState
     );
+
+    useEffect(() => {
+        // Caching here in localstorage so it doesn't make a network call that's already made previously
+        async function getProfileInfoAndPhotos() {
+            const [{ ...user }] = await getUserByUsername(username);
+        }
+        getProfileInfoAndPhotos();
+    }, [username]);
 
     return (
         <>
