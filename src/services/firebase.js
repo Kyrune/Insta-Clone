e969c0cada8b2b039fabf339bc38ac85aae353aa
Kyrune <jn-01@centurytel.net>
2021-02-24 13:11:55 -0600

    Optimize getSuggestedProfiles functionality

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -54,12 +54,24 @@ export async function getUserFollowedPhotos(userId, followingUserIds) {
 
 export async function getSuggestedProfiles(userId) {
     const result = await firebase.firestore().collection('users').limit(10).get();
-    // Destructure
-    const [{ following: userFollowing = []}] = result.docs
-        .map((user) => user.data())
-        .filter((profile) => profile.userId === userId);
-
-        return result.docs
-            .map((user) => ({ ...user.data(), docId: user.id }))
-            .filter((profile) => profile.userId !== userId && !userFollowing.includes(profile.userId))
+    // Destructure for users the user is following
+    const [{ following }] = await getUserByUserId(userId);
+        
+    return result.docs
+        .map((user) => ({ ...user.data(), docId: user.id }))
+        // Filters out user's own profile in suggestions
+        .filter((profile) => profile.userId !== userId && !following.includes(profile.userId));
 }
+
+// export async function getSuggestedProfiles(userId) {
+//     const result = await firebase.firestore().collection('users').limit(10).get();
+//     // Destructure for users the user is following
+//     const [{ following: userFollowing = []}] = result.docs
+//         .map((user) => user.data())
+//         .filter((profile) => profile.userId === userId);
+
+//         return result.docs
+//             .map((user) => ({ ...user.data(), docId: user.id }))
+//             // Filters out user's own profile in suggestions
+//             .filter((profile) => profile.userId !== userId && !userFollowing.includes(profile.userId));
+// }
